9501ae9fba7c3ed0b4061c5b1e150bf3a9129e29
Gaia Micciancio <gaiamicciancio@gmail.com>
2024-09-11 23:02:01 -0400

    chchanges based on assignment

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,5 +1,19 @@
 public class Multiples {
     public static void main(String[] args) {
+        int i = 0;
+        int j = 1;
+
+        while (j < 1000){
+            boolean divisibleBya = j % 3 == 0;
+            boolean divisibleByb = j % 5 == 0;
+            if (divisibleBya) {
+                i++;
+            } else if (divisibleByb) {
+                i++;
+            }
+            j++;
+        }
+        System.out.println(i);
         System.out.println(466);
     }
 }
--- src/Reduce.java
+++ src/Reduce.java
@@ -1,5 +1,18 @@
 public class Reduce {
     public static void main(String[] args) {
+        int i = 0;
+        int g = 100;
+        while (g > 0) {
+            i++;
+            boolean even = g % 2 == 0;
+            if (even) {
+                g = (g / 2);
+            } else {
+                g--;
+            }
+        }
+        System.out.println(i);
+
         System.out.println(9);
     }
 }
